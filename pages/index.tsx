09dfade0097f7a3845aaa2cedc8e1fe12713db73
oda <ooddaa@gmail.com>
2022-06-22 12:05:02 +0100

    [Passport] submit button below, minor styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
-// import Passport from "./components/Passport"
-import Passport from "./components/Passport2"
+import Passport from "./components/Passport"
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
